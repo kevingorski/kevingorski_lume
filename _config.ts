3e415ccb3c18a86fb65b7c63d7c3a16a268a50e3
Kevin Gorski <kevin.gorski@gmail.com>
2024-01-14 15:55:51 -0700

    Apply Lume blog template and upgrade to Lume v2

---
+++ _config.ts
@@ -0,0 +1,46 @@
+import lume from "lume/mod.ts";
+import date from "lume/plugins/date.ts";
+import postcss from "lume/plugins/postcss.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
+import basePath from "lume/plugins/base_path.ts";
+import slugifyUrls from "lume/plugins/slugify_urls.ts";
+import resolveUrls from "lume/plugins/resolve_urls.ts";
+import pageFind from "lume/plugins/pagefind.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import feed from "lume/plugins/feed.ts";
+
+const site = lume({
+  location: new URL("https://kevingorski.com/"),
+});
+
+site
+  .ignore("README.md")
+  .copy("img")
+  .use(postcss())
+  .use(date())
+  .use(codeHighlight())
+  .use(basePath())
+  .use(sitemap())
+  .use(pageFind({
+    ui: {
+      resetStyles: false,
+    },
+  }))
+  .use(slugifyUrls({ alphanumeric: false }))
+  .use(feed({
+    output: ["/feed.json", "/feed.xml"],
+    query: "type=posts",
+    info: {
+      description: "=site.description",
+      title: "=site.title",
+    },
+    items: {
+      content: "$.post-body",
+      title: "=title",
+      updated: "=date",
+    },
+  }))
+  .use(resolveUrls())
+  
+export default site;
+
---
+++ about.md
@@ -0,0 +1,10 @@
+---
+layout: layouts/post.vto
+menu:
+  visible: true
+  order: 2
+title: About Me
+templateClass: tmpl-post
+---
+
+I am a person that writes stuff.
